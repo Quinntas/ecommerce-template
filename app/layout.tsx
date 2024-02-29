6118e623a6b6bbdef14db4a27dea52b86e01bf57
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2024-02-28 21:04:13 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import {cn} from "@/lib/utils";
 import {ThemeProvider} from "@/lib/providers/theme-provider";
 import {Toaster} from "@/components/ui/sonner"
+import {Analytics} from "@vercel/analytics/react"
 
 const fontSans = FontSans({
     subsets: ["latin"],
@@ -38,6 +39,7 @@ export default function RootLayout({
             {children}
             <Toaster/>
         </ThemeProvider>
+        <Analytics/>
         </body>
         </html>
     );
